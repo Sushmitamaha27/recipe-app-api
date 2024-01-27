bf261f8e5b11115570b4b7632a1030e8be212168
Sushmitamaha27 <sushmitagmahajan@gmail.com>
2024-01-27 23:21:47 +0530

    Add filename to the repository

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+'''
+Calculate functions
+'''
+
+
+def add(x,y):
+    '''Returning the adding value.'''
+    return x+y
+
+
+def subtract(x,y):
+    return y-x
---
+++ app/app/test.py
@@ -0,0 +1,24 @@
+'''
+sample tests
+'''
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the Calc module."""
+    
+    def test_add_number(self):
+        """Test adding number together."""
+        res=calc.add(5,6)
+        
+        self.assertEqual(res,11)
+        
+    def subtract_number(self):
+        """Testing subtract number"""
+        res=calc.subtract(10,15)
+        
+
+        self.assertEqual(res,5)
